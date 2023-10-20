ead6c7913f97f80a28f476816b9d9e09c3e0cb9e
Will-Tate <103454866+Will-Tate@users.noreply.github.com>
2023-10-20 10:22:18 -0400

    lets hope nothing breaks

---
+++ Assets/Scripts/Playercount.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//public Text Count;
+//public int playCount = 0;
+public class Playercount : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Count.text = ""
+            //+ playCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*public void ChangeText()
+    {
+        playCount++;
+        playCount.text = "" + playCount;
+    }*/
+}
