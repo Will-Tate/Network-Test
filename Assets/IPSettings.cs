1a80cdd5d22eea83fc48a763572e377d3282b430
Will-Tate <103454866+Will-Tate@users.noreply.github.com>
2023-11-13 23:40:51 -0500

    Updated to work with local networks

---
+++ Assets/IPSettings.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
+
+public class IPSettings : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI ipAddressText;
+    [SerializeField] TMP_InputField ip;
+    [SerializeField] string ipAddress;
+    [SerializeField] UnityTransport transport;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ipAddress = "0.0.0.0";
+        SetIpAddress(); // Set the Ip to the above address
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NetworkManager.Singleton.IsHost && !NetworkManager.Singleton.IsClient)
+        {
+            GetLocalIPAddress();
+        }
+        }
+    public string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                ipAddressText.text = ip.ToString();
+                ipAddress = ip.ToString();
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    public void SetIpAddress()
+    {
+        ipAddress = ip.text;
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = ipAddress;
+    }
+}
--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
 namespace HelloWorld
 {
     public class HelloWorldManager : MonoBehaviour
     {
+        /*
+        [SerializeField] TMP_InputField ip;
+        [SerializeField] TextMeshProUGUI ipAddressText;
+        [SerializeField] UnityTransport transport;
+        [SerializeField] string ipAddress;
+        */
         void OnGUI()
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
@@ -26,8 +36,18 @@ namespace HelloWorld
 
         static void StartButtons()
         {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Host"))
+            {
+                NetworkManager.Singleton.StartHost();
+                //GetLocalIPAddress();
+            }
+            if (GUILayout.Button("Client"))
+            {
+                //ipAddress = ;//ip.text;
+                //UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+                //transport.ConnectionData.Address = "128.61.25.245";//ipAddress;
+                NetworkManager.Singleton.StartClient();
+            }
             if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
         }
 
@@ -41,6 +61,30 @@ namespace HelloWorld
             GUILayout.Label("Mode: " + mode);
         }
 
+        /*
+        //HOST ONLY
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddressText.text = ip.ToString();
+                    ipAddress = ip.ToString();
+                    return ip.ToString();
+                }
+            }
+            throw new System.Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        // ONLY FOR CLIENT SIDE
+        public void SetIpAddress()
+        {
+            transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.ConnectionData.Address = ipAddress;
+        }
+        */
         static void SubmitNewPosition()
         {
             if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
